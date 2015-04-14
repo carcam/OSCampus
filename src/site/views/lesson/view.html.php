225eef15cf0393cd8a3faee3fe8715d173b92080
Bill Tomczak <bill@ostraining.com>
2015-04-14 15:47:43 -0700

    Create primary view stubs for frontend

---
+++ src/site/views/lesson/view.html.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusViewLesson extends OscampusViewSite
+{
+}
