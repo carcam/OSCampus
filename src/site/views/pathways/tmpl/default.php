af5685c6461758c7470958a65593353fe2fe9b42
Valentín <htmgarcia@gmail.com>
2015-04-20 18:47:17 -0500

    Starter structure for pathways view

--- src/site/views/pathways/tmpl/default.php
+++ src/site/views/pathways/tmpl/default.php
@@ -11,3 +11,43 @@ defined('_JEXEC') or die();
 <h1>Under Construction</h1>
 This page is meant to be equivalent to
 <a href="https://www.ostraining.com/courses/" target="_blank">Online Training Classes</a>
+
+<div class="ost-container oscampus-pathways">
+
+    <div class="ost-section">
+        <div class="block4">
+            <a href="#">
+                <img src="https://www.ostraining.com/cdn/images/stories/guru/categories/thumbs/WordPress.jpg" alt="Category Image">
+            </a>
+        </div>
+        <div class="block8">
+            WordPress is easy to use and easy to modify. For these reasons, WordPress is the most popular way for people to build a website.  Millions of people every day use it to create blogs and websites.
+        </div>
+    </div>
+    <!-- .ost-section -->
+
+    <div class="ost-section">
+        <div class="block4">
+            <a href="#">
+                <img src="https://www.ostraining.com/cdn/images/stories/guru/categories/thumbs/WordPress.jpg" alt="Category Image">
+            </a>
+        </div>
+        <div class="block8">
+            WordPress is easy to use and easy to modify. For these reasons, WordPress is the most popular way for people to build a website.  Millions of people every day use it to create blogs and websites.
+        </div>
+    </div>
+    <!-- .ost-section -->
+
+    <div class="ost-section">
+        <div class="block4">
+            <a href="#">
+                <img src="https://www.ostraining.com/cdn/images/stories/guru/categories/thumbs/WordPress.jpg" alt="Category Image">
+            </a>
+        </div>
+        <div class="block8">
+            WordPress is easy to use and easy to modify. For these reasons, WordPress is the most popular way for people to build a website.  Millions of people every day use it to create blogs and websites.
+        </div>
+    </div>
+    <!-- .ost-section -->
+
+</div>
