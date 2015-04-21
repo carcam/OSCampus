d8c408bb45034e5b4b6edb0cac454a0eb0b3fb85
Bill Tomczak <bill@ostraining.com>
2015-04-21 10:18:41 -0700

    Don't allow modules or lessons to be reusable, remove difficulty level from lessons

--- src/admin/sql/install/mysql/uninstall.sql
+++ src/admin/sql/install/mysql/uninstall.sql
@@ -1,12 +1,11 @@
 DROP TABLE IF EXISTS `ext53_oscampus_courses_pathways`;
-DROP TABLE IF EXISTS `ext53_oscampus_courses_modules`;
-DROP TABLE IF EXISTS `ext53_oscampus_modules_lessons`;
+DROP TABLE IF EXISTS `ext53_oscampus_pathways`;
+
 DROP TABLE IF EXISTS `ext53_oscampus_students_lessons`;
 DROP TABLE IF EXISTS `ext53_oscampus_certificates`;
-DROP TABLE IF EXISTS `ext53_oscampus_lessons`;
-DROP TABLE IF EXISTS `ext53_oscampus_pathways`;
-DROP TABLE IF EXISTS `ext53_oscampus_courses`;
 DROP TABLE IF EXISTS `ext53_oscampus_students`;
+
+DROP TABLE IF EXISTS `ext53_oscampus_lessons`;
 DROP TABLE IF EXISTS `ext53_oscampus_modules`;
-DROP TABLE IF EXISTS `ext53_oscampus_levels`;
+DROP TABLE IF EXISTS `ext53_oscampus_courses`;
 DROP TABLE IF EXISTS `ext53_oscampus_instructors`;
