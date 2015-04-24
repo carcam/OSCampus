e4a0179a35b1a2771bd3c353c2c2487f9722096c
Bill Tomczak <bill@ostraining.com>
2015-04-24 15:59:01 -0700

    Create links to single course

--- src/site/views/pathway/tmpl/default.php
+++ src/site/views/pathway/tmpl/default.php
@@ -9,6 +9,9 @@
 defined('_JEXEC') or die();
 
 /** @var OscampusViewPathway $this */
+
+$itemId = JFactory::getApplication()->input->getInt('Itemid');
+$itemId = $itemId ? '&Itemid=' . $itemId : '';
 ?>
 
 <div class="osc-container oscampus-pathway">
@@ -20,7 +23,7 @@ defined('_JEXEC') or die();
     <?php
     foreach ($this->items as $item) :
         $image = JHtml::_('image', $item->image, $item->title);
-        $link = '#';
+        $link = 'index.php?option=com_oscampus&view=course&cid=' . $item->id . $itemId;
     ?>
     <div class="osc-section osc-course-item">
         <div class="block4 osc-course-image">
