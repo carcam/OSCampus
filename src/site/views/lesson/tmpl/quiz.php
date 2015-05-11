a71265ee661ad3da85ba3d99caee30e6c634943b
Valentín <htmgarcia@gmail.com>
2015-05-11 18:54:10 -0500

    Add quiz layout

---
+++ src/site/views/lesson/tmpl/quiz.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+?>
+
+<div class="osc-container oscampus-lesson">
+
+    <div class="page-header">
+        <h1>Quiz name</h1>
+    </div>
+
+</div>
