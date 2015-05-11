d35fded147125db2313fb5b4aa0f30aaf4d32b54
Valentín <htmgarcia@gmail.com>
2015-05-11 18:35:37 -0500

    Hide text in small screens

--- src/site/views/lesson/tmpl/default.php
+++ src/site/views/lesson/tmpl/default.php
@@ -37,15 +37,13 @@ This will be specific lessons. Right now this means either (login at ostraining.
                 <div class="osc-btn-group wistia_buttons_container" id="wistia_NaN_buttons_container">
                     <a href="#" class="osc-btn-main wistia_button download hidden" id="wistia_NaN_download_button">
                         <i class="fa fa-cloud-download"></i>
-                        <span class="uk-hidden-small uk-hidden-medium"> Download</span>
-                    </a>
-                    <a href="#" class="osc-btn-main wistia_button autoplay hidden" id="wistia_NaN_autoplay_button">
+                        <span class="osc-hide-tablet"> Download</span>
+                    </a><a href="#" class="osc-btn-main wistia_button autoplay hidden" id="wistia_NaN_autoplay_button">
                         <i class="fa fa-check"></i>
-                        <span class="uk-hidden-small uk-hidden-medium">Autoplay</span>
-                    </a>
-                    <a href="#" class="osc-btn-main osc-btn-active wistia_button focus hidden" id="wistia_NaN_focus_button">
+                        <span class="osc-hide-tablet">Autoplay</span>
+                    </a><a href="#" class="osc-btn-main osc-btn-active wistia_button focus hidden" id="wistia_NaN_focus_button">
                         <i class="fa fa-times"></i>
-                        <span class="uk-hidden-small uk-hidden-medium">Focus</span>
+                        <span class="osc-hide-tablet">Focus</span>
                     </a>
                 </div>
             </div>
@@ -53,14 +51,12 @@ This will be specific lessons. Right now this means either (login at ostraining.
                 <div class="osc-btn-group hidden" id="course-navigation">
                     <a href="#" class="osc-btn-main">
                         <i class="fa fa-bars"></i>
-                        <span class="uk-hidden-small uk-hidden-medium">Home</span>
-                    </a>
-                    <a href="#" class="osc-btn-main" id="prevbut">
+                        <span class="osc-hide-tablet">Home</span>
+                    </a><a href="#" class="osc-btn-main" id="prevbut">
                         <i class="fa fa-angle-left"></i>
-                        <span class="uk-hidden-small uk-hidden-medium">Prev</span>
-                    </a>
-                    <a href="#" class="osc-btn-main" id="nextbut">
-                        <span class="uk-hidden-small uk-hidden-medium">Next</span>
+                        <span class="osc-hide-tablet">Prev</span>
+                    </a><a href="#" class="osc-btn-main" id="nextbut">
+                        <span class="osc-hide-tablet">Next</span>
                         <i class="fa fa-angle-right"></i>
                     </a>
                 </div>
