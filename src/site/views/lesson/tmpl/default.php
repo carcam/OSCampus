59bddb70c1e77a5a2f9c6c957701ca4d13b90b8a
Valentín <htmgarcia@gmail.com>
2015-05-11 16:41:13 -0500

    Add lesson view

--- src/site/views/lesson/tmpl/default.php
+++ src/site/views/lesson/tmpl/default.php
@@ -24,3 +24,15 @@ This will be specific lessons. Right now this means either (login at ostraining.
         </a>
     </li>
 </ul>
+
+<div class="osc-container oscampus-lesson">
+
+    <div class="page-header">
+        <h1>Lesson name</h1>
+    </div>
+
+    <div class="osc-lesson-embed">
+        <?php echo JHTML::_('content.prepare', '{wistia}bbql2oyrgi{/wistia}'); ?>
+    </div>
+
+</div>
