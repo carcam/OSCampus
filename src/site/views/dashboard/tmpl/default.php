5487a408d5a570b0e7e311e4393004dac218f3b4
Valentín <htmgarcia@gmail.com>
2015-05-14 18:44:07 -0500

    Tabs for dashboard

--- src/site/views/dashboard/tmpl/default.php
+++ src/site/views/dashboard/tmpl/default.php
@@ -8,4 +8,85 @@
 
 defined('_JEXEC') or die();
 ?>
-<h1>Under Construction</h1>
+
+<div class="osc-container oscampus-dashboard">
+
+    <div class="page-header">
+        <h1>Dashboard</h1>
+    </div>
+
+    <div class="osc-section">
+        <div class="block3">
+            Lorem
+        </div>
+        <div class="block3">
+            Lorem
+        </div>
+        <div class="block3">
+            Lorem
+        </div>
+        <div class="block3">
+            Lorem
+        </div>
+    </div>
+    <!-- .osc-section -->
+
+    <div class="osc-section osc-dashboard-tabs">
+        <div class="block4">
+            <div class="osc-section osc-course-tabs">
+                <div data-content="#content-my-classes" class="block12">
+                    My Classes
+                </div>
+                <div data-content="#content-my-certificates" class="block12 osc-tab-disabled">
+                    My Certificates
+                </div>
+                <div data-content="#content-my-forum" class="block12 osc-tab-disabled">
+                    My Forum Topics
+                </div>
+                <div data-content="#content-my-stats" class="block12 osc-tab-disabled">
+                    My Stats
+                </div>
+                <div data-content="#content-new-classes" class="block12 osc-tab-disabled">
+                    New Classes
+                </div>
+                <div data-content="#content-new-blog" class="block12 osc-tab-disabled">
+                    New on the Blog
+                </div>
+            </div>
+            <!-- .osc-section -->
+        </div>
+        <div class="block8">
+            <div id="content-my-classes" class="osc-course-tabs-content">
+                My Classes
+            </div>
+            <!-- #content- -->
+
+            <div id="content-my-certificates" class="osc-course-tabs-content osc-course-tabs-content-right" style="display: none">
+                My Certificates
+            </div>
+            <!-- #content- -->
+
+            <div id="content-my-forum" class="osc-course-tabs-content osc-course-tabs-content-right" style="display: none">
+                My Forum Topics
+            </div>
+            <!-- #content- -->
+
+            <div id="content-my-stats" class="osc-course-tabs-content osc-course-tabs-content-right" style="display: none">
+                My Stats
+            </div>
+            <!-- #content- -->
+
+            <div id="content-new-classes" class="osc-course-tabs-content osc-course-tabs-content-right" style="display: none">
+                New Classes
+            </div>
+            <!-- #content- -->
+
+            <div id="content-new-blog" class="osc-course-tabs-content osc-course-tabs-content-right" style="display: none">
+                New on the Blog
+            </div>
+            <!-- #content- -->
+        </div>
+    </div>
+    <!-- .osc-section -->
+
+</div>
