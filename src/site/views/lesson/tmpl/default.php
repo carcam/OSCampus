3b14d71d9efeb36bab317ff4afd0f56b3359fdbb
Valentín <htmgarcia@gmail.com>
2015-05-14 14:41:15 -0500

    Remove unnecessary text

--- src/site/views/lesson/tmpl/default.php
+++ src/site/views/lesson/tmpl/default.php
@@ -8,22 +8,6 @@
 
 defined('_JEXEC') or die();
 ?>
-<h1>Under Construction</h1>
-This will be specific lessons. Right now this means either (login at ostraining.com required)
-
-<ul>
-    <li>
-        <a href="https://www.ostraining.com/courses/session/wordpress/why/videos/introduction/" target="_blank">
-            a video
-        </a>
-        or
-    </li>
-    <li>
-        <a href="https://www.ostraining.com/courses/session/wordpress/why/videos/quiz/" target="_blank: ">
-            a quiz
-        </a>
-    </li>
-</ul>
 
 <div class="osc-container oscampus-lesson">
 
