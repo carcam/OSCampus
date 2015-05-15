9793fc191f83b15305dfd9c93f4a46441f436562
Valentín <htmgarcia@gmail.com>
2015-05-15 15:23:10 -0500

    Announcements area in dashboard

--- src/site/views/dashboard/tmpl/default.php
+++ src/site/views/dashboard/tmpl/default.php
@@ -17,18 +17,18 @@ JHtml::_('osc.tabs', '.osc-course-tabs div');
         <h1>Dashboard</h1>
     </div>
 
-    <div class="osc-section">
+    <div class="osc-section osc-dashboard-announcements">
         <div class="block3">
-            Lorem
+            You've completed <strong>3 certificates</strong> and watched <strong>387 videos</strong>
         </div>
         <div class="block3">
-            Lorem
+            The most watched class past week was <a href="javascript:void(0)">Gimp 4: Fills, Gradients, Masking</a>
         </div>
         <div class="block3">
-            Lorem
+            Do you need help? <a href="javascript:void(0)">Ask for assitance in the forum</a>
         </div>
-        <div class="block3">
-            Lorem
+        <div class="block3 osc-dashboard-block-featured">
+            Sign up in Alledia, <strong>30% off</strong> with coupon <strong>OSTMEMBER</strong>
         </div>
     </div>
     <!-- .osc-section -->
