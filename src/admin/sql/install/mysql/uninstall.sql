4d1991500c01df3e8026de01f9a21e09738bf942
Bill Tomczak <bill@ostraining.com>
2015-06-02 16:11:46 -0700

    Move to standard db install/uninstall and stop using FK constraints

--- src/admin/sql/install/mysql/uninstall.sql
+++ src/admin/sql/install/mysql/uninstall.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS `hfps_oscampus_courses_pathways`;
-DROP TABLE IF EXISTS `hfps_oscampus_pathways`;
+DROP TABLE IF EXISTS `#__oscampus_courses_pathways`;
+DROP TABLE IF EXISTS `#__oscampus_pathways`;
 
-DROP TABLE IF EXISTS `hfps_oscampus_courses_tags`;
-DROP TABLE IF EXISTS `hfps_oscampus_tags`;
+DROP TABLE IF EXISTS `#__oscampus_courses_tags`;
+DROP TABLE IF EXISTS `#__oscampus_tags`;
 
-DROP TABLE IF EXISTS `hfps_oscampus_users_lessons`;
-DROP TABLE IF EXISTS `hfps_oscampus_certificates`;
+DROP TABLE IF EXISTS `#__oscampus_users_lessons`;
+DROP TABLE IF EXISTS `#__oscampus_certificates`;
 
-DROP TABLE IF EXISTS `hfps_oscampus_files_lessons`;
-DROP TABLE IF EXISTS `hfps_oscampus_files_courses`;
-DROP TABLE IF EXISTS `hfps_oscampus_files`;
+DROP TABLE IF EXISTS `#__oscampus_files_lessons`;
+DROP TABLE IF EXISTS `#__oscampus_files_courses`;
+DROP TABLE IF EXISTS `#__oscampus_files`;
 
-DROP TABLE IF EXISTS `hfps_oscampus_lessons`;
-DROP TABLE IF EXISTS `hfps_oscampus_modules`;
-DROP TABLE IF EXISTS `hfps_oscampus_courses`;
-DROP TABLE IF EXISTS `hfps_oscampus_teachers`;
+DROP TABLE IF EXISTS `#__oscampus_lessons`;
+DROP TABLE IF EXISTS `#__oscampus_modules`;
+DROP TABLE IF EXISTS `#__oscampus_courses`;
+DROP TABLE IF EXISTS `#__oscampus_teachers`;
