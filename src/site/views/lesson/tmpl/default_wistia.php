0a1655556662a359dd7b3fe63f1d86c3fbc69c6f
Bill Tomczak <bill@ostraining.com>
2015-06-10 12:35:37 -0700

    Add language strings

--- src/site/views/lesson/tmpl/default_wistia.php
+++ src/site/views/lesson/tmpl/default_wistia.php
@@ -17,24 +17,40 @@ if (!empty($this->lesson->content->id)) :
     <div class="osc-btn-group wistia_buttons_container" id="wistia_NaN_buttons_container">
         <a href="#" class="osc-btn wistia_button download hidden" id="wistia_NaN_download_button">
             <i class="fa fa-cloud-download"></i>
-            <span class="osc-hide-tablet"> Download</span>
-        </a><a href="#" class="osc-btn wistia_button autoplay hidden" id="wistia_NaN_autoplay_button">
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_DOWNLOAD'); ?>
+            </span>
+        </a>
+        <a href="#" class="osc-btn wistia_button autoplay hidden" id="wistia_NaN_autoplay_button">
             <i class="fa fa-check"></i>
-            <span class="osc-hide-tablet">Autoplay</span>
-        </a><a href="#" class="osc-btn osc-btn-active wistia_button focus hidden" id="wistia_NaN_focus_button">
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_AUTOPLAY'); ?>
+            </span>
+        </a>
+        <a href="#" class="osc-btn osc-btn-active wistia_button focus hidden" id="wistia_NaN_focus_button">
             <i class="fa fa-times"></i>
-            <span class="osc-hide-tablet">Focus</span>
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_FOCUS'); ?>
+            </span>
         </a>
     </div>
     <div class="osc-btn-group hidden" id="course-navigation">
         <a href="#" class="osc-btn">
             <i class="fa fa-bars"></i>
-            <span class="osc-hide-tablet">Home</span>
-        </a><a href="#" class="osc-btn" id="prevbut">
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_HOME'); ?>
+            </span>
+        </a>
+        <a href="#" class="osc-btn" id="prevbut">
             <i class="fa fa-chevron-left"></i>
-            <span class="osc-hide-tablet">Prev</span>
-        </a><a href="#" class="osc-btn" id="nextbut">
-            <span class="osc-hide-tablet">Next</span>
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_PREVIOUS'); ?>
+            </span>
+        </a>
+        <a href="#" class="osc-btn" id="nextbut">
+            <span class="osc-hide-tablet">
+                <?php echo JText::_('COM_OSCAMPUS_NEXT'); ?>
+            </span>
             <i class="fa fa-chevron-right"></i>
         </a>
     </div>
