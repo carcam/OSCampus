a4ec5db313ed2632148dc0a5fc859e272d2f5945
Bill Tomczak <bill@ostraining.com>
2015-06-10 12:36:05 -0700

    Cache current lesson results

--- src/site/models/lesson.php
+++ src/site/models/lesson.php
@@ -12,61 +12,66 @@ defined('_JEXEC') or die();
 
 class OscampusModelLesson extends OscampusModelSite
 {
+    protected $lesson = null;
+
     public function getLesson()
     {
-        $cid = (int)$this->getState('course.id');
-        $idx = (int)$this->getState('lesson.index');
+        if ($this->lesson === null) {
+            $cid = (int)$this->getState('course.id');
+            $idx = (int)$this->getState('lesson.index');
 
-        $db = $this->getDbo();
+            $db = $this->getDbo();
 
-        $query = $db->getQuery(true)
-            ->select('l.*')
-            ->from('#__oscampus_lessons l')
-            ->innerJoin('#__oscampus_modules m ON m.id = l.modules_id')
-            ->where('m.courses_id = ' . $cid)
-            ->order('m.ordering, l.ordering');
+            $query = $db->getQuery(true)
+                ->select('l.*')
+                ->from('#__oscampus_lessons l')
+                ->innerJoin('#__oscampus_modules m ON m.id = l.modules_id')
+                ->where('m.courses_id = ' . $cid)
+                ->order('m.ordering, l.ordering');
 
-        $offset = max(0, $idx - 1);
-        $limit  = $idx ? 3 : 2;
-        $data   = $db->setQuery($query, $offset, $limit)->loadObjectList();
+            $offset = max(0, $idx - 1);
+            $limit  = $idx ? 3 : 2;
+            $data   = $db->setQuery($query, $offset, $limit)->loadObjectList();
 
-        foreach ($data as $lesson) {
-            if ($lesson->type != 'text') {
-                $lesson->content = json_decode($lesson->content);
+            foreach ($data as $lesson) {
+                if ($lesson->type != 'text') {
+                    $lesson->content = json_decode($lesson->content);
+                }
             }
+
+            if (count($data) === 3) {
+                list($previous, $this->lesson, $next) = $data;
+            } elseif ($idx == 0) {
+                $previous = null;
+                list($this->lesson, $next) = $data;
+            } else {
+                list($previous, $this->lesson) = $data;
+                $next = null;
+            }
+
+            $this->lesson->index    = $idx;
+            $this->lesson->previous = $previous;
+            $this->lesson->next     = $next;
         }
 
-        if (count($data) === 3) {
-            list($previous, $lesson, $next) = $data;
-        } elseif ($idx == 0) {
-            $previous = null;
-            list($lesson, $next) = $data;
-        } else {
-            list($previous, $lesson) = $data;
-            $next = null;
-        }
-
-        $lesson->index    = $idx;
-        $lesson->previous = $previous;
-        $lesson->next     = $next;
-
-        return $lesson;
+        return $this->lesson;
     }
 
     public function getFiles()
     {
-        $lid = (int)$this->getState('lesson.id');
-        $db  = $this->getDbo();
+        if ($lesson = $this->getLesson()) {
+            $db  = $this->getDbo();
+            $query = $db->getQuery(true)
+                ->select('f.*')
+                ->from('#__oscampus_files f')
+                ->innerJoin('#__oscampus_files_lessons fl ON fl.files_id = f.id')
+                ->where('fl.lessons_id = ' . $lesson->id);
 
-        $query = $db->getQuery(true)
-            ->select('f.*')
-            ->from('#__oscampus_files f')
-            ->innerJoin('#__oscampus_files_lessons fl ON fl.files_id = f.id')
-            ->where('fl.lessons_id = ' . $lid);
+            $files = $db->setQuery($query)->loadObjectList();
+            return $files;
+        }
 
-        $files = $db->setQuery($query)->loadObjectList();
-
-        return $files;
+        return array();
     }
 
     protected function populateState()
