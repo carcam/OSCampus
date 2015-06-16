9c5bd19ffcd75f074aa88f3c2804baad9903f781
Bill Tomczak <bill@ostraining.com>
2015-06-16 11:26:29 -0700

    Remove references to obsolete classes

--- src/admin/library/joomla/factory.php
+++ src/admin/library/joomla/factory.php
@@ -6,35 +6,9 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 
-use Oscampus\Cms\Joomla\Services\Oscampus;
-use Oscampus\Container;
-
 defined('_JEXEC') or die();
 
 abstract class OscampusFactory extends JFactory
 {
-    /**
-     * @var array
-     */
-    protected static $OscampusContainer = null;
 
-    /**
-     * Get the Oscampus container class
-     *
-     * @param JRegistry $params
-     *
-     * @return Container
-     * @throws Exception
-     */
-    public static function getContainer(JRegistry $params = null)
-    {
-        if (static::$OscampusContainer === null) {
-            $container = new Container();
-            $services  = new Oscampus(array());
-            $container->register($services);
-
-            static::$OscampusContainer = $container;
-        }
-        return static::$OscampusContainer;
-    }
 }
