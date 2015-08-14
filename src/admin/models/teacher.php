dbc87a02e2841ed947a93599dbb97e75dd7232a0
Anderson Grudtner Martins <anderson@ostraining.com>
2015-08-13 23:59:53 -0300

    Add base for teacher's form

---
+++ src/admin/models/teacher.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+
+class OscampusModelTeacher extends OscampusModelAdmin
+{
+    public function getTable($type = 'Teachers', $prefix = 'OscampusTable', $config = array())
+    {
+        return OscampusTable::getInstance($type, $prefix, $config);
+    }
+
+    public function getForm($data = array(), $loadData = true)
+    {
+        $form = $this->loadForm('com_oscampus.teacher', 'teacher', array('control' => 'jform', 'load_data' => $loadData));
+        if (empty($form)) {
+            return false;
+        }
+
+        return $form;
+    }
+
+    protected function loadFormData()
+    {
+        $data = OscampusFactory::getApplication()->getUserState('com_oscampus.edit.teacher.data', array());
+
+        if (empty($data)) {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+
+    /**
+     * @param JTable $table
+     *
+     * @return void
+     */
+    protected function prepareTable($table)
+    {
+
+    }
+}
---
+++ src/admin/views/teacher/view.html.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusViewTeacher extends OscampusViewAdmin
+{
+    protected $item;
+
+    public function display($tpl = null)
+    {
+        $state = $this->get('State');
+
+        $this->item = $this->get('Item');
+
+        $this->setVariable('list_order', $this->escape($state->get('list.ordering')));
+        $this->setVariable('list_dir', $this->escape($state->get('list.direction')));
+        $this->setVariable('form', $this->get('Form'));
+        $this->setVariable('item', $this->item);
+
+        parent::display($tpl);
+    }
+
+    /**
+     * Default admin screen title
+     *
+     * @param string $sub
+     * @param string $icon
+     *
+     * @return void
+     */
+    protected function setTitle($sub = null, $icon = 'oscampus')
+    {
+        $isNew = ($this->item->id == 0);
+        $title = 'COM_OSCAMPUS_PAGE_VIEW_TEACHER_' . ($isNew ? 'ADD' : 'EDIT');
+
+        parent::setTitle($title, $icon);
+    }
+
+    protected function setToolbar()
+    {
+        $isNew = ($this->item->id == 0);
+        OscampusFactory::getApplication()->input->set('hidemainmenu', true);
+
+        OscampusToolbarHelper::apply('teacher.apply');
+        OscampusToolbarHelper::save('teacher.save');
+
+        $alt = $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE';
+        OscampusToolbarHelper::cancel('teacher.cancel', $alt);
+
+        parent::setToolbar();
+    }
+}
