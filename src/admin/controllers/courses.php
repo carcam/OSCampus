c80a0b432aac8b13243b8f4fb5834b40abbed620
Anderson Grudtner Martins <anderson@ostraining.com>
2015-08-17 17:13:06 -0300

    Add initial files for the course list and form

---
+++ src/admin/controllers/courses.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusControllerCourses extends OscampusControllerAdmin
+{
+    protected $text_prefix = 'COM_OSCAMPUS_COURSES';
+
+    public function getModel($name = 'Course', $prefix = 'OscampusModel', $config = array('ignore_request' => true))
+    {
+        $model = parent::getModel($name, $prefix, $config);
+        return $model;
+    }
+}
---
+++ src/admin/models/courses.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+
+class OscampusModelCourses extends OscampusModelList
+{
+    public function __construct($config = array())
+    {
+        $config['filter_fields'] = array(
+            'id', 'course.id',
+            'title', 'course.title',
+            'published', 'course.published'
+        );
+
+        parent::__construct($config);
+    }
+
+    protected function getListQuery()
+    {
+        $db = $this->getDbo();
+
+        $query = $db->getQuery(true);
+        $query->select(
+            array(
+                'course.*',
+                'course.checked_out as editor'
+            )
+        );
+        $query->from('#__oscampus_courses course');
+
+        if ($search = $this->getState('filter.search')) {
+            $search = $db->q('%' . $search . '%');
+            $ors    = array(
+                'course.title like ' . $search,
+                'course.id like ' . $search
+            );
+            $query->where('(' . join(' OR ', $ors) . ')');
+        }
+
+        $listOrder = $this->getState('list.ordering', 'course.id');
+        $listDir   = $this->getState('list.direction', 'ASC');
+        $query->order($listOrder . ' ' . $listDir);
+
+        return $query;
+    }
+
+    protected function populateState($ordering = null, $direction = null)
+    {
+        $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+        $this->setState('filter.search', $search);
+
+        parent::populateState('course.id', 'ASC');
+    }
+}
