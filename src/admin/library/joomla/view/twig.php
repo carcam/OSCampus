25b6fbb231b765084ef7e4a6f5b9a14e1d82ca0a
Bill Tomczak <bill@ostraining.com>
2015-11-24 15:42:56 -0800

    Reformat copied code to psr2

--- src/admin/library/joomla/view/twig.php
+++ src/admin/library/joomla/view/twig.php
@@ -29,14 +29,15 @@ class OscampusViewTwig extends OscampusView
     /**
      * Constructor
      *
-     * @param   array  $config  A named configuration array for object construction.<br />
-     *                          name: the name (optional) of the view (defaults to the view class name suffix).<br />
-     *                          charset: the character set to use for display<br />
-     *                          escape: the name (optional) of the function to use for escaping strings<br />
-     *                          base_path: the parent path (optional) of the views directory (defaults to the component folder)<br />
-     *                          template_plath: the path (optional) of the layout directory (defaults to base_path + /views/ + view name<br />
-     *                          helper_path: the path (optional) of the helper files (defaults to base_path + /helpers/)<br />
-     *                          layout: the layout (optional) to use to display the view<br />
+     * @param   array $config A named configuration array for object construction.<br />
+     *                        name: the name (optional) of the view (defaults to the view class name suffix).<br />
+     *                        charset: the character set to use for display<br />
+     *                        escape: the name (optional) of the function to use for escaping strings<br />
+     *                        base_path: the parent path (optional) of the views directory (defaults to the component
+     *                        folder)<br /> template_plath: the path (optional) of the layout directory (defaults to
+     *                        base_path + /views/ + view name<br /> helper_path: the path (optional) of the helper
+     *                        files (defaults to base_path + /helpers/)<br /> layout: the layout (optional) to use to
+     *                        display the view<br />
      *
      * @since   12.2
      */
@@ -53,7 +54,7 @@ class OscampusViewTwig extends OscampusView
 
         $this->_addPath('template', JPATH_COMPONENT . '/layouts');
 
-        $loader  = new Twig_Loader_Filesystem($this->_path['template']);
+        $loader = new Twig_Loader_Filesystem($this->_path['template']);
 
         $options = array(
             // 'cache' => JPATH_CACHE,
@@ -71,7 +72,8 @@ class OscampusViewTwig extends OscampusView
     /**
      * Load a template file -- first look in the templates folder for an override
      *
-     * @param   string  $tpl  The name of the template source file; automatically searches the template paths and compiles as needed.
+     * @param   string $tpl The name of the template source file; automatically searches the template paths and
+     *                      compiles as needed.
      *
      * @return  string  The output of the the template script.
      *
@@ -83,8 +85,8 @@ class OscampusViewTwig extends OscampusView
         // Clear prior output
         $this->_output = null;
 
-        $template = OscampusFactory::getApplication()->getTemplate();
-        $layout = $this->getLayout();
+        $template       = OscampusFactory::getApplication()->getTemplate();
+        $layout         = $this->getLayout();
         $layoutTemplate = $this->getLayoutTemplate();
 
         // Create the template file name based on the layout
@@ -92,48 +94,43 @@ class OscampusViewTwig extends OscampusView
 
         // Clean the file name
         $file = preg_replace('/[^A-Z0-9_\.-]/i', '', $file);
-        $tpl = isset($tpl) ? preg_replace('/[^A-Z0-9_\.-]/i', '', $tpl) : $tpl;
+        $tpl  = isset($tpl) ? preg_replace('/[^A-Z0-9_\.-]/i', '', $tpl) : $tpl;
 
         // Load the language file for the template
         $lang = OscampusFactory::getLanguage();
         $lang->load('tpl_' . $template, JPATH_BASE, null, false, true)
-            || $lang->load('tpl_' . $template, JPATH_THEMES . "/$template", null, false, true);
+        || $lang->load('tpl_' . $template, JPATH_THEMES . "/$template", null, false, true);
 
         // Change the template folder if alternative layout is in different template
-        if (isset($layoutTemplate) && $layoutTemplate != '_' && $layoutTemplate != $template)
-        {
+        if (isset($layoutTemplate) && $layoutTemplate != '_' && $layoutTemplate != $template) {
             $this->_path['template'] = str_replace($template, $layoutTemplate, $this->_path['template']);
         }
 
         // Load the template script
         jimport('joomla.filesystem.path');
-        $filetofind = $this->_createFileName('template', array('name' => $file));
+        $filetofind      = $this->_createFileName('template', array('name' => $file));
         $this->_template = JPath::find($this->_path['template'], $filetofind);
 
         // If alternate layout can't be found, fall back to default layout
-        if ($this->_template == false)
-        {
-            $filetofind = $this->_createFileName('', array('name' => 'default' . (isset($tpl) ? '_' . $tpl : $tpl)));
+        if ($this->_template == false) {
+            $filetofind      = $this->_createFileName('',
+                array('name' => 'default' . (isset($tpl) ? '_' . $tpl : $tpl)));
             $this->_template = JPath::find($this->_path['template'], $filetofind);
         }
 
-        if ($this->_template != false)
-        {
+        if ($this->_template != false) {
             // Unset so as not to introduce into template scope
             unset($tpl);
             unset($file);
 
             // Never allow a 'this' property
-            if (isset($this->this))
-            {
+            if (isset($this->this)) {
                 unset($this->this);
             }
 
             // Renders the template
             return $this->templatesEngine->render(basename($this->_template), $this->variables);
-        }
-        else
-        {
+        } else {
             throw new Exception(JText::sprintf('JLIB_APPLICATION_ERROR_LAYOUTFILE_NOT_FOUND', $file), 500);
         }
     }
@@ -141,8 +138,8 @@ class OscampusViewTwig extends OscampusView
     /**
      * Create the filename for a resource
      *
-     * @param   string  $type   The resource type to create the filename for
-     * @param   array   $parts  An associative array of filename information
+     * @param   string $type  The resource type to create the filename for
+     * @param   array  $parts An associative array of filename information
      *
      * @return  string  The filename
      *
@@ -150,8 +147,7 @@ class OscampusViewTwig extends OscampusView
      */
     protected function _createFileName($type, $parts = array())
     {
-        switch ($type)
-        {
+        switch ($type) {
             case 'template':
                 $filename = strtolower($parts['name']) . '.' . $this->_layoutExt;
                 break;
@@ -167,8 +163,8 @@ class OscampusViewTwig extends OscampusView
     /**
      * Set variables for the template
      *
-     * @param  string|array  $name
-     * @param  mixed         $value
+     * @param  string|array $name
+     * @param  mixed        $value
      *
      * @return void
      */
