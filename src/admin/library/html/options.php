65d6ec9e7cf210c29d1961805087e02e3ab3e806
Bill Tomczak <bill@ostraining.com>
2015-11-24 10:35:54 -0800

    Add a teacher form field

--- src/admin/library/html/options.php
+++ src/admin/library/html/options.php
@@ -12,6 +12,11 @@ defined('_JEXEC') or die();
 
 abstract class OscOptions
 {
+    /**
+     * Get option list of course difficulty levels
+     *
+     * @return object[]
+     */
     public static function difficulty()
     {
         $options = array(
@@ -33,5 +38,37 @@ abstract class OscOptions
         );
 
         return $options;
+    }
+
+    /**
+     * Get option list of known teachers
+     *
+     * @return object[]
+     */
+    public static function teachers()
+    {
+        $db = OscampusFactory::getDbo();
+        $db->escape('');
+
+        $query = $db->getQuery(true)
+            ->select(
+                array(
+                    'teacher.id',
+                    'user.username',
+                    'user.name'
+                )
+            )
+            ->from('#__oscampus_teachers teacher')
+            ->innerJoin('#__users user ON user.id = teacher.users_id')
+            ->order('user.username');
+
+        $teachers = array_map(
+            function($row) {
+                return JHtml::_('select.option', $row->id, sprintf('%s (%s)', $row->username, $row->name));
+            },
+            $db->setQuery($query)->loadObjectList()
+        );
+
+        return $teachers;
     }
 }
