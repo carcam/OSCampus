d2d7f1cda59c9cef82e55b94b33cb82983b71b25
Bill Tomczak <bill@ostraining.com>
2015-11-24 09:54:19 -0800

    Set some defaults for new courses

--- src/admin/models/course.php
+++ src/admin/models/course.php
@@ -11,5 +11,17 @@ defined('_JEXEC') or die();
 
 class OscampusModelCourse extends OscampusModelAdmin
 {
+    public function getItem($pk = null)
+    {
+        $item = parent::getItem($pk);
 
+        if (!$item->id) {
+            $item->released = date('Y-m-d');
+            $item->access = 1;
+            $item->published = 1;
+            $item->difficulty = \Oscampus\Course::BEGINNER;
+        }
+
+        return $item;
+    }
 }
