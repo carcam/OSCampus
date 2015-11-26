7ad846b5ac16fe8d545f8a16557f735aa5faf69d
Bill Tomczak <bill@ostraining.com>
2015-11-26 15:52:12 -0800

    Fix a few minor issues with course listing

--- src/admin/models/courses.php
+++ src/admin/models/courses.php
@@ -14,14 +14,14 @@ class OscampusModelCourses extends OscampusModelList
     public function __construct($config = array())
     {
         $config['filter_fields'] = array(
-            'id',            'course.id',
-            'title',         'course.title',
-            'tags',          'tag.title',
-            'pathways',      'pathway.title',
-            'difficulty',    'course.difficulty',
-            'published',     'course.published',
-            'access_level',  'course.access',
-            'teachers_name', 'user.name'
+            'id',              'course.id',
+            'title',           'course.title',
+            'tags',            'tag.title',
+            'pathways',        'pathway.title',
+            'difficulty',      'course.difficulty',
+            'published',       'course.published',
+            'viewlevel_title', 'viewlevel.title',
+            'teacher_name',    'teacher_user.name'
         );
 
         parent::__construct($config);
@@ -35,9 +35,8 @@ class OscampusModelCourses extends OscampusModelList
         $query->select(
             array(
                 'course.*',
-                'course.checked_out as editor',
-                'user.name as teachers_name',
-                'viewlevel.title as access_level',
+                'teacher_user.name as teacher_name',
+                'viewlevel.title as viewlevel_title',
                 'GROUP_CONCAT(DISTINCT tag.title) AS tags',
                 'GROUP_CONCAT(DISTINCT pathway.title) AS pathways',
                 'editor_user.name editor'
@@ -45,7 +44,7 @@ class OscampusModelCourses extends OscampusModelList
         )
             ->from('#__oscampus_courses course')
             ->leftJoin('#__oscampus_teachers teacher ON course.teachers_id = teacher.id')
-            ->leftJoin('#__users user ON teacher.users_id = user.id')
+            ->leftJoin('#__users teacher_user ON teacher.users_id = teacher_user.id')
             ->leftJoin('#__viewlevels viewlevel ON course.access = viewlevel.id')
             ->leftJoin('#__oscampus_courses_tags course_tags ON course.id = course_tags.courses_id')
             ->leftJoin('#__oscampus_tags tag ON course_tags.tags_id = tag.id')
@@ -64,14 +63,12 @@ class OscampusModelCourses extends OscampusModelList
 
         $query->group('course.id');
 
+        $primary = $this->getState('list.ordering', 'course.title');
         $direction = $this->getState('list.direction', 'ASC');
-        $ordering = array(
-            $this->getState('list.ordering', 'course.id') . ' ' . $direction,
-            'tag.title ' . $direction,
-            'pathway.title ' . $direction
-        );
-        $query->order($ordering);
-
+        $query->order($primary . ' ' . $direction);
+        if (!in_array($primary, array('course.title', 'course.id'))) {
+            $query->order('course.title ' . $direction);
+        }
 
         return $query;
     }
