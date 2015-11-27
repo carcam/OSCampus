d7b87bbb609399f3b90bd49a302d74ad86fec989
Bill Tomczak <bill@ostraining.com>
2015-11-27 06:09:46 -0800

    Standardize utilities and macros for twig form rendering

--- src/admin/library/html/render.php
+++ src/admin/library/html/render.php
@@ -10,6 +10,16 @@ defined('_JEXEC') or die();
 
 abstract class OscRender
 {
+    /**
+     * @var int[] Provide conversion from grid spans to J!2 widths
+     */
+    protected static $spanWidths = array(20, 20, 30, 40, 40, 50, 60, 60, 70, 80, 80, 100);
+
+    /**
+     * When in J!3 display the submenu as a sidebar. J!2 has no equivalent.
+     *
+     * @return string
+     */
     public static function submenu()
     {
         if (version_compare(JVERSION, '3', 'ge')) {
@@ -20,30 +30,94 @@ abstract class OscRender
     }
 
     /**
-     * Render a form fieldset for the admin UI
+     * Convert a grid span to a J!2 width class number
      *
-     * @param JForm  $form
-     * @param string $fieldSet
-     * @param string $legend
-     * @param string $width
+     * @param int|string $span
      *
      * @return string
      */
-    public static function adminfieldset(JForm $form, $fieldSet, $legend = null, $width = null)
+    protected static function spanToWidth($span)
+    {
+        $idx = $span - 1;
+        if (isset(static::$spanWidths[$idx])) {
+            return (string)static::$spanWidths[$idx];
+        }
+
+        return '100';
+    }
+
+    /**
+     * Write a J! version agnostic fieldset block for the admin UI
+     *
+     * @param JForm       $form
+     * @param string      $fieldSet
+     * @param bool|string $legend
+     * @param int|string  $span
+     *
+     * @return string
+     */
+    public static function adminfieldset(JForm $form, $fieldSet, $legend = false, $span = 12)
     {
         $html      = array();
         $fieldSets = $form->getFieldsets();
 
         if (!empty($fieldSets[$fieldSet])) {
-            $legend = $legend ?: $fieldSets[$fieldSet]->label;
+            if ($legend === true) {
+                $legend = $fieldSets[$fieldSet]->label;
+            }
             $fields = $form->getFieldset($fieldSet);
 
             if (version_compare(JVERSION, '3.0', 'lt')) {
-                $html = static::adminFieldsetJ2($fields, $legend);
+                $html = static::adminFieldsetJ2($fields, $legend, $span);
             } else {
-                $html = static::adminFieldsetJ3($fields, $legend);
+                $html = static::adminFieldsetJ3($fields, $legend, $span);
             }
         }
+        return join("\n", $html);
+    }
+
+    /**
+     * Write a J! version agnostic field block for the admin UI
+     *
+     * @param JFormField  $field
+     * @param bool|string $legend
+     * @param int|string  $span
+     *
+     * @return string
+     */
+    public static function adminfield(JFormField $field, $legend = false, $span = 12)
+    {
+        if ($legend === true) {
+            if (version_compare(JVERSION, '3', 'lt')) {
+                // The only way to get what we want in J!2
+                $elementProperty = new ReflectionProperty($field, 'element');
+                $elementProperty->setAccessible(true);
+
+                $element = $elementProperty->getValue($field);
+                $legend  = JText::_($element['label']);
+
+            } else {
+                $legend = $field->getAttribute('label');
+            }
+        }
+
+        if (version_compare(JVERSION, '3', 'lt')) {
+            $class = array('fltlft', 'width-' . static::spanToWidth($span));
+        } else {
+            $class = array('span-' . $span);
+        }
+
+        $html   = array();
+        $html[] = '<div class="' . join(' ', $class) . '">';
+        $html[] = '<fieldset class="adminform">';
+
+        if ($legend) {
+            $html[] = '<legend>' . JText::_($legend) . '</legend>';
+        }
+        $html[] = $field->input;
+        $html[] = '</fieldset>';
+        $html[] = '</div>';
+
         return join("\n", $html);
     }
 
@@ -66,17 +140,18 @@ abstract class OscRender
     }
 
     /**
-     * Render a Joomla! 3 fieldset for admin UI
+     * Render a fieldset block for Joomla! 3 admin UI
      *
      * @param JFormFIeld[] $fields
-     * @param string       $legend
+     * @param bool|string  $legend
+     * @param int|string   $span
      *
      * @return array
      */
-    protected static function adminFieldsetJ3(array $fields, $legend = null)
+    protected static function adminFieldsetJ3(array $fields, $legend, $span)
     {
         $html   = array();
-        $html[] = '<div class="row-fluid">';
+        $html[] = '<div class="row-fluid span-' . $span . '">';
         $html[] = '<fieldset class="adminform">';
         if ($legend) {
             $html[] = '<legend>' . JText::_($legend) . '</legend>';
@@ -104,7 +179,6 @@ abstract class OscRender
 
         $html[] = '<ul class="adminformlist">';
 
-        reset($fields);
         $count = 0;
         foreach ($fields as $name => $field) {
             $count++;
@@ -141,20 +215,18 @@ abstract class OscRender
     }
 
     /**
-     * Render a fieldset for Joomla! 2 admin UI
+     * Render a fieldset block for Joomla! 2 admin UI
      *
      * @param JFormField[] $fields
-     * @param string       $legend
-     * @param string       $width
+     * @param bool|string  $legend
+     * @param int|string   $span
      *
      * @return array
      */
-    protected static function adminFieldsetJ2(array $fields, $legend = null, $width = null)
+    protected static function adminFieldsetJ2(array $fields, $legend, $span)
     {
-        $width = $width ?: '100';
-
         $html   = array();
-        $html[] = '<div class="width-' . $width . '">';
+        $html[] = '<div class="fltlft width-' . static::spanToWidth($span) . '">';
         $html[] = '<fieldset class="adminform">';
 
         if ($legend) {
@@ -165,7 +237,6 @@ abstract class OscRender
 
         $html[] = '</fieldset>';
         $html[] = '</div>';
-        $html[] = '<div class="clr"></div>';
 
         return $html;
     }
