f6c25a508fdc8141e1d7a2efb1c85074a8c37d70
Bill Tomczak <bill@ostraining.com>
2015-12-03 14:45:41 -0800

    Add lesson form stub

---
+++ src/admin/controllers/lessons.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2015 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusControllerLessons extends OscampusControllerAdmin
+{
+    protected $text_prefix = 'COM_OSCAMPUS_LESSONS';
+
+    public function getModel($name = 'Lesson', $prefix = 'OscampusModel', $config = array('ignore_request' => true))
+    {
+        $model = parent::getModel($name, $prefix, $config);
+        return $model;
+    }
+}
