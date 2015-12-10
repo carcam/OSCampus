00a6f58abde248f409e01005c164eea4c6a726ad
Bill Tomczak <bill@ostraining.com>
2015-12-10 13:02:25 -0800

    Be more restrictive on when lesson ordering is enabled

--- src/admin/views/lessons/view.html.php
+++ src/admin/views/lessons/view.html.php
@@ -21,7 +21,7 @@ class OscampusViewLessons extends OscampusViewList
             array(
                 'field'   => 'lesson.ordering',
                 'prefix'  => 'lessons.',
-                'enabled' => $state->get('list.ordering') == 'lesson.ordering'
+                'enabled' => $this->enableOrdering()
             )
         );
         $this->setVariable('ordering', $ordering);
@@ -67,6 +67,29 @@ class OscampusViewLessons extends OscampusViewList
         );
 
         $this->setVariable('filters', $filters);
+    }
+
+    /**
+     * Determine whether it's okay to enable ordering
+     *
+     * @return bool
+     */
+    protected function enableOrdering()
+    {
+        $state = $this->getState();
+
+        $enabled = false;
+        if ($state->get('list.ordering') == 'lesson.ordering') {
+            $enabled = true;
+            $stateVars = $state->getProperties();
+            foreach ($stateVars as $name => $value) {
+                if ($value && strpos($name, 'filter.') === 0 && $name != 'filter.course') {
+                    return false;
+                }
+            }
+        }
+
+        return $enabled;
     }
 
     public function getSortGroupId($item)
