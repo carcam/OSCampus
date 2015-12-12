0753c6bba6535fa3b11b59f4136a8cfd670c6574
Bill Tomczak <bill@ostraining.com>
2015-12-11 20:04:32 -0800

    Fix strange formatting issues

--- src/admin/models/lessons.php
+++ src/admin/models/lessons.php
@@ -7,119 +7,125 @@
  */
 
 defined('_JEXEC') or die();
- class OscampusModelLessons extends OscampusModelList
- {
-     public function __construct($config = array())
-     {
-         $config['filter_fields'] = array(
-             'id',                'lesson.id',
-             'title',             'lesson.title',
-             'type',              'lesson.type',
-             'published',         'lesson.published',
-             'module_title',      'module.title',
-             'module_published',  'module.published',
-             'course_title',      'course.title',
-             'course_published',  'course.published',
-             'course_released',   'course.released',
-             'course_defficulty', 'course.difficulty',
-             'lesson_ordering',   'lesson.ordering',
-             'viewlevel_title',   'lesson_view.title'
-         );
 
-         parent::__construct($config);
-     }
+class OscampusModelLessons extends OscampusModelList
+{
+    public function __construct($config = array())
+    {
+        $config['filter_fields'] = array(
+            'id',                'lesson.id',
+            'title',             'lesson.title',
+            'type',              'lesson.type',
+            'published',         'lesson.published',
+            'module_title',      'module.title',
+            'module_published',  'module.published',
+            'course_title',      'course.title',
+            'course_published',  'course.published',
+            'course_released',   'course.released',
+            'course_defficulty', 'course.difficulty',
+            'lesson_ordering',   'lesson.ordering',
+            'viewlevel_title',   'lesson_view.title'
+        );
 
-     protected function getListQuery()
-     {
-         $db = $this->getDbo();
+        parent::__construct($config);
+    }
 
-         $query = $db->getQuery(true)
-             ->select(
-                 array(
-                     'lesson.id',
-                     'lesson.modules_id',
-                     'module.courses_id',
-                     'module.ordering AS module_ordering',
-                     'lesson.ordering',
-                     'lesson.title',
-                     'lesson.alias',
-                     'lesson.type',
-                     'lesson.published',
-                     'lesson_view.title AS viewlevel_title',
-                     'module.title AS module_title',
-                     'module.published AS module_published',
-                     'course.title AS course_title',
-                     'course.published AS course_published',
-                     'course.released AS course_released',
-                     'course.difficulty AS course_difficulty'
-                 )
-             )
-             ->from('#__oscampus_lessons lesson')
-             ->leftJoin('#__oscampus_modules AS module ON module.id = lesson.modules_id')
-             ->leftJoin('#__oscampus_courses AS course ON course.id = module.courses_id')
-             ->leftJoin('#__viewlevels AS lesson_view ON lesson_view.id = lesson.access')
-             ->leftJoin('#__viewlevels AS course_view ON course_view.id = course.access');
+    protected function getListQuery()
+    {
+        $db = $this->getDbo();
 
-         $this->whereTextSearch($query, 'lesson.id', 'lesson.title', 'lesson.alias');
+        $query = $db->getQuery(true)
+            ->select(
+                array(
+                    'lesson.id',
+                    'lesson.modules_id',
+                    'module.courses_id',
+                    'module.ordering AS module_ordering',
+                    'lesson.ordering',
+                    'lesson.title',
+                    'lesson.alias',
+                    'lesson.type',
+                    'lesson.published',
+                    'lesson_view.title AS viewlevel_title',
+                    'module.title AS module_title',
+                    'module.published AS module_published',
+                    'course.title AS course_title',
+                    'course.published AS course_published',
+                    'course.released AS course_released',
+                    'course.difficulty AS course_difficulty'
+                )
+            )
+            ->from('#__oscampus_lessons lesson')
+            ->leftJoin('#__oscampus_modules AS module ON module.id = lesson.modules_id')
+            ->leftJoin('#__oscampus_courses AS course ON course.id = module.courses_id')
+            ->leftJoin('#__viewlevels AS lesson_view ON lesson_view.id = lesson.access')
+            ->leftJoin('#__viewlevels AS course_view ON course_view.id = course.access');
 
-         $published = $this->getState('filter.published');
-         if ($published != '') {
-             $query->where('lesson.published = ' . (int)$published);
-         }
+        $this->whereTextSearch($query, 'lesson.id', 'lesson.title', 'lesson.alias');
 
-         if ($course = (int)$this->getState('filter.course')) {
-             $query->where('course.id = ' . $course);
-         }
+        $published = $this->getState('filter.published');
+        if ($published != '') {
+            $query->where('lesson.published = ' . (int)$published);
+        }
 
-         if ($lessonType = $this->getState('filter.lessontype')) {
-             $query->where('lesson.type = ' . $db->q($lessonType));
-         }
+        if ($course = (int)$this->getState('filter.course')) {
+            $query->where('course.id = ' . $course);
+        }
 
-         if ($access = (int)$this->getState('filter.access')) {
-             $query->where('lesson.access = ' . $access);
-         }
+        if ($lessonType = $this->getState('filter.lessontype')) {
+            $query->where('lesson.type = ' . $db->q($lessonType));
+        }
 
-         $primary = $this->getState('list.ordering', 'course.title');
-         $direction = $this->getState('list.direction', 'ASC');
-         if ($primary == 'lesson.ordering') {
-             $query->order(
-                 array(
-                     'module.ordering ' . $direction,
-                     'course.title ' . $direction
-                 )
-             );
-         }
-         $query->order($primary . ' ' . $direction);
+        if ($access = (int)$this->getState('filter.access')) {
+            $query->where('lesson.access = ' . $access);
+        }
 
-         if (!in_array($primary, array('lesson.title', 'lesson.id', 'lesson.ordering'))) {
-             $query->order('lesson.title ' . $direction);
-         }
+        $primary   = $this->getState('list.ordering', 'course.title');
+        $direction = $this->getState('list.direction', 'ASC');
+        if ($primary == 'lesson.ordering') {
+            $query->order(
+                array(
+                    'module.ordering ' . $direction,
+                    'course.title ' . $direction
+                )
+            );
+        }
+        $query->order($primary . ' ' . $direction);
 
-         return $query;
-     }
+        if (!in_array($primary, array('lesson.title', 'lesson.id', 'lesson.ordering'))) {
+            $query->order('lesson.title ' . $direction);
+        }
 
-     protected function populateState($ordering = 'lesson.title', $direction = 'ASC')
-     {
-         $app = OscampusFactory::getApplication();
-         if ($app->input->getBool('clear', false)) {
-             $app->setUserState($this->context, null);
-         }
+        return $query;
+    }
 
-         $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search', '', 'string');
-         $this->setState('filter.search', $search);
+    protected function populateState($ordering = 'lesson.title', $direction = 'ASC')
+    {
+        $app = OscampusFactory::getApplication();
+        if ($app->input->getBool('clear', false)) {
+            $app->setUserState($this->context, null);
+        }
 
-         $published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published');
-         $this->setState('filter.published', $published);
+        $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search', '', 'string');
+        $this->setState('filter.search', $search);
 
-         $course = $this->getUserStateFromRequest($this->context . '.filter.course', 'filter_course', null, 'int');
-         $this->setState('filter.course', $course);
+        $published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published');
+        $this->setState('filter.published', $published);
 
-         $lessonType = $this->getUserStateFromRequest($this->context . '.filter.lessontype', 'filter_lessontype', '', 'string');
-         $this->setState('filter.lessontype', $lessonType);
+        $course = $this->getUserStateFromRequest($this->context . '.filter.course', 'filter_course', null, 'int');
+        $this->setState('filter.course', $course);
 
-         $access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access');
-         $this->setState('filter.access', $access);
+        $lessonType = $this->getUserStateFromRequest(
+            $this->context . '.filter.lessontype',
+            'filter_lessontype',
+            '',
+            'string'
+        );
+        $this->setState('filter.lessontype', $lessonType);
 
-         parent::populateState($ordering, $direction);
-     }
- }
+        $access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access');
+        $this->setState('filter.access', $access);
+
+        parent::populateState($ordering, $direction);
+    }
+}
