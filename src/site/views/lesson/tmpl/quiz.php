482920872ff10dca4eac98da6c6c8dd8394de370
Valentín <htmgarcia@gmail.com>
2016-01-06 11:07:55 -0600

    Add classes to quiz form
    Issue #57

--- src/site/views/lesson/tmpl/quiz.php
+++ src/site/views/lesson/tmpl/quiz.php
@@ -25,20 +25,22 @@ $questions = $quiz->getQuestions();
         </div>
     </div>
 
-    <div class="osc-section" style="text-align: center; margin: 15px;">
+    <div class="osc-section osc-quiz-details">
         <div class="block4">
-            <div id="oscampus-timer">
-                <div class="oscampus-timer-header" style="margin-bottom: 5px;">
+            <div id="oscampus-timer" class="osc-quiz-left">
+                <span class="osc-quiz-score-label">
                     <?php echo JText::_('COM_OSCAMPUS_QUIZ_TIME_LEFT'); ?>
-                </div>
-                <div class="oscampus-timer-countdown" style="font-size: 50px;">
+                </span><br/>
+                <span class="osc-quiz-percentage">
                     10:00
-                </div>
+                </span><br/>
             </div>
         </div>
         <div class="block4">
-            <span style="font-weight: bold;"><?php echo JText::_('COM_OSCAMPUS_QUIZ_PASSING_SCORE'); ?></span>
-            <?php echo $quiz->passingScore . '%'; ?>
+            <div class="osc-quiz-right">
+                <strong><?php echo JText::_('COM_OSCAMPUS_QUIZ_PASSING_SCORE'); ?></strong>
+                <strong class="osc-positive-color"><?php echo $quiz->passingScore . '%'; ?></strong>
+            </div>
         </div>
     </div>
 
@@ -49,29 +51,27 @@ $questions = $quiz->getQuestions();
     <?php endif; ?>
 
     <div class="osc-section oscampus-lesson-content">
-        <ul>
-            <?php
-            $qn = 0;
-            foreach ($questions as $qkey => $question):
-                ?>
-                <li class="<?php echo 'question' . ($qn++ % 2); ?>">
-                    <span><?php echo sprintf('Q%s: %s', $qn, $question->text); ?></span>
-                    <ul>
-                        <?php
-                        $an = 0;
-                        foreach ($question->answers as $akey => $answer):
-                            ?>
-                            <li><input
-                                    type="radio"
-                                    name="<?php echo $qkey; ?>"
-                                    value="<?php echo $akey; ?>"/>
-                                <label><?php echo $answer->text; ?></label>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <?php
+        $qn = 0;
+        foreach ($questions as $qkey => $question):
+            ?>
+            <div class="<?php echo 'question' . ($qn++ % 2); ?> osc-quiz-question">
+                <h4><?php echo sprintf('Q%s: %s', $qn, $question->text); ?></h4>
+                <ul class="osc-quiz-options">
+                    <?php
+                    $an = 0;
+                    foreach ($question->answers as $akey => $answer):
+                        ?>
+                        <li><input
+                                type="radio"
+                                name="<?php echo $qkey; ?>"
+                                value="<?php echo $akey; ?>"/>
+                            <?php echo $answer->text; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endforeach; ?>
     </div>
 
     <?php if ($this->lesson->footer) : ?>
