01c3753929c0276dd62589d4a2bf88460442fc7f
Bill Tomczak <bill@ostraining.com>
2016-01-07 11:36:05 -0800

    Code reformat

--- src/site/views/course/tmpl/default_teacher.php
+++ src/site/views/course/tmpl/default_teacher.php
@@ -32,40 +32,40 @@ $routing = OscampusRoute::getInstance();
     </div>
     <?php
     if ($this->teacher->courses) :
-    ?>
-    <h3><?php echo JText::_('COM_OSCAMPUS_TEACHER_COURSES'); ?></h3>
-    <div class="osc-table">
-        <div class="osc-section osc-row-heading osc-hide-tablet">
-            <div class="block6">
-                <i class="fa fa-bars"></i> <?php echo JText::_('COM_OSCAMPUS_TEACHER_NAME'); ?>
-            </div>
-            <div class="block3">
-                <i class="fa fa-signal"></i> <?php echo JText::_('COM_OSCAMPUS_COURSE_DIFFICULTY'); ?>
-            </div>
-            <div class="block3">
-                <i class="fa fa-calendar"></i> <?php echo JText::_('COM_OSCAMPUS_COURSE_RELEASE_DATE') ;?>
-            </div>
-        </div>
-        <?php
-        foreach ($this->teacher->courses as $i => $course) :
-            $courseLink = JRoute::_($routing->get('courses') . '&view=course&cid=' . $course->id);
-            ?>
-            <div class="<?php echo 'osc-section ' . ($i % 2 ? 'osc-row-two' : 'osc-row-one'); ?>">
+        ?>
+        <h3><?php echo JText::_('COM_OSCAMPUS_TEACHER_COURSES'); ?></h3>
+        <div class="osc-table">
+            <div class="osc-section osc-row-heading osc-hide-tablet">
                 <div class="block6">
-                    <?php echo JHtml::_('link', $courseLink, $course->title); ?>
+                    <i class="fa fa-bars"></i> <?php echo JText::_('COM_OSCAMPUS_TEACHER_NAME'); ?>
                 </div>
-                <div class="block3 osc-hide-tablet">
-                    <?php echo JText::_('COM_OSCAMPUS_DIFFICULTY_' . $course->difficulty); ?>
+                <div class="block3">
+                    <i class="fa fa-signal"></i> <?php echo JText::_('COM_OSCAMPUS_COURSE_DIFFICULTY'); ?>
                 </div>
-                <div class="block3 osc-hide-tablet">
-                    <?php echo JHtml::_('date', $course->released, 'F j, Y'); ?>
+                <div class="block3">
+                    <i class="fa fa-calendar"></i> <?php echo JText::_('COM_OSCAMPUS_COURSE_RELEASE_DATE'); ?>
                 </div>
             </div>
             <?php
-        endforeach;
-        ?>
-    </div>
-    <?php
+            foreach ($this->teacher->courses as $i => $course) :
+                $courseLink = JRoute::_($routing->get('courses') . '&view=course&cid=' . $course->id);
+                ?>
+                <div class="<?php echo 'osc-section ' . ($i % 2 ? 'osc-row-two' : 'osc-row-one'); ?>">
+                    <div class="block6">
+                        <?php echo JHtml::_('link', $courseLink, $course->title); ?>
+                    </div>
+                    <div class="block3 osc-hide-tablet">
+                        <?php echo JText::_('COM_OSCAMPUS_DIFFICULTY_' . $course->difficulty); ?>
+                    </div>
+                    <div class="block3 osc-hide-tablet">
+                        <?php echo JHtml::_('date', $course->released, 'F j, Y'); ?>
+                    </div>
+                </div>
+                <?php
+            endforeach;
+            ?>
+        </div>
+        <?php
     endif;
     ?>
 </div>
