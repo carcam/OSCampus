6758f67850c0a82870ae023062ca50f0c3193298
Bill Tomczak <bill@ostraining.com>
2016-01-09 14:15:03 -0800

    Reorganize quiz templates

--- src/site/views/lesson/tmpl/quiz.php
+++ src/site/views/lesson/tmpl/quiz.php
@@ -50,37 +50,7 @@ $questions = $quiz->getQuestions();
         </div>
     <?php endif; ?>
 
-    <div class="osc-section oscampus-lesson-content">
-        <?php
-        $qn = 0;
-        foreach ($questions as $qkey => $question):
-            ?>
-            <div class="<?php echo 'question' . ($qn++ % 2); ?> osc-quiz-question">
-                <h4><?php echo sprintf('Q%s: %s', $qn, $question->text); ?></h4>
-                <ul class="osc-quiz-options">
-                    <?php
-                    $an = 0;
-                    foreach ($question->answers as $akey => $answer):
-                        ?>
-                        <li><input
-                                type="radio"
-                                name="<?php echo $qkey; ?>"
-                                value="<?php echo $akey; ?>"/>
-                            <?php echo $answer->text; ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        <?php endforeach; ?>
-    </div>
-    <!-- .osc-section -->
-
-    <div class="osc-section osc-quiz-submit">
-        <button class="osc-btn osc-btn-main" type="submit">
-            <?php echo JText::_('COM_OSCAMPUS_QUIZ_SUBMIT'); ?>
-        </button>
-    </div>
-    <!-- .osc-section -->
+    <?php echo $this->loadTemplate('form'); ?>
 
     <?php if ($this->lesson->footer) : ?>
         <div class="osc-section oscampus-lesson-footer">
--- src/site/views/lesson/tmpl/quiz_form.php
+++ src/site/views/lesson/tmpl/quiz_form.php
@@ -6,31 +6,47 @@
  * @license
  */
 
+/**
+ * @var OscampusViewLesson $this
+ */
 defined('_JEXEC') or die();
+/**
+ * @var Oscampus\Lesson\Type\Quiz $quiz
+ */
+$quiz      = $this->lesson->render();
+$questions = $quiz->getQuestions();
 
 ?>
-<h2>Form</h2>
-
-<form name="osc_quiz" method="post" action="">
-    <?php foreach ($this->lesson->content->questions as $idx => $question): ?>
-        <div class="osc-quiz-question">
-            <h4><?php echo $question->text; ?></h4>
-            <ul class="osc-quiz-options">
-                <?php foreach ($question->choices as $choice): ?>
-                    <li>
-                        <input
-                            type="radio"
-                            name="<?php echo "answers[{$idx}]"; ?>"
-                            value="<?php echo $choice->text; ?>"/>
-                        <?php echo $choice->text; ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    <?php endforeach; ?>
+<form id="quizForm" name="quizForm" action="javascript:alert('under construction');">
+    <div class="osc-section oscampus-lesson-content">
+        <?php
+        $qn = 0;
+        foreach ($questions as $qkey => $question):
+            ?>
+            <div class="<?php echo 'question' . ($qn++ % 2); ?> osc-quiz-question">
+                <h4><?php echo sprintf('Q%s: %s', $qn, $question->text); ?></h4>
+                <ul class="osc-quiz-options">
+                    <?php
+                    $an = 0;
+                    foreach ($question->answers as $akey => $answer):
+                        ?>
+                        <li><input
+                                type="radio"
+                                name="<?php echo $qkey; ?>"
+                                value="<?php echo $akey; ?>"/>
+                            <?php echo $answer->text; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <!-- .osc-section -->
 
-    <div>
-        <button type="submit" class="osc-btn osc-btn-main">Submit</button>
+    <div class="osc-section osc-quiz-submit">
+        <button class="osc-btn osc-btn-main" type="submit">
+            <?php echo JText::_('COM_OSCAMPUS_QUIZ_SUBMIT'); ?>
+        </button>
     </div>
+    <!-- .osc-section -->
 </form>
