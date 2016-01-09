809571ad466001e14f9148320865732bbda9987d
Bill Tomczak <bill@ostraining.com>
2016-01-09 14:27:43 -0800

    More compact format for lesson js

--- src/media/js/lesson.js
+++ src/media/js/lesson.js
@@ -66,37 +66,34 @@
      * @param {object} options
      */
 
-    $.Oscampus.lesson = $.extend($.Oscampus.lesson, {
-        previous: null,
-        current : null,
-        next    : null,
+    $.Oscampus.lesson.previous = null;
+    $.Oscampus.lesson.current = null;
+    $.Oscampus.lesson.next = null;
+    $.Oscampus.lesson.navigation = function(options) {
+        this.previous = options.previous || null;
+        this.current = options.current || null;
+        this.next = options.next || null;
 
-        navigation: function(options) {
-            this.previous = options.previous || null;
-            this.current = options.current || null;
-            this.next = options.next || null;
+        var container  = $('#oscampus.osc-container'),
+            setLoading = function(message, title) {
+                container
+                    .addClass('loading')
+                    .html('<span class="message">' + message.replace('%s', title) + '</span>');
+                document.title = Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_TITLE');
+            };
 
-            var container  = $('#oscampus.osc-container'),
-                setLoading = function(message, title) {
-                    container
-                        .addClass('loading')
-                        .html('<span class="message">' + message.replace('%s', title) + '</span>');
-                    document.title = Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_TITLE');
-                };
-
-            if (this.next && this.next.title) {
-                var next = this.next;
-                $('#nextbut').on('click', function(evt) {
-                    setLoading(Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_NEXT'), next.title);
-                });
-            }
-
-            if (this.previous && this.previous.title) {
-                var previous = this.previous;
-                $('#prevbut').on('click', function(evt) {
-                    setLoading(Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_PREVIOUS'), previous.title);
-                });
-            }
+        if (this.next && this.next.title) {
+            var next = this.next;
+            $('#nextbut').on('click', function(evt) {
+                setLoading(Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_NEXT'), next.title);
+            });
         }
-    });
+
+        if (this.previous && this.previous.title) {
+            var previous = this.previous;
+            $('#prevbut').on('click', function(evt) {
+                setLoading(Joomla.JText._('COM_OSCAMPUS_LESSON_LOADING_PREVIOUS'), previous.title);
+            });
+        }
+    };
 })(jQuery);
