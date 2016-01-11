691a3e2a32af0560fd2a9b1e4c54e427919d5d84
Bill Tomczak <bill@ostraining.com>
2016-01-11 12:50:28 -0800

    Add My Courses view stub

---
+++ src/site/views/mycourses/view.html.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2016 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusViewMycourses extends OscampusViewSite
+{
+
+}
