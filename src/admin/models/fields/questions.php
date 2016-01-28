f0d2cdaf23dee33e69ba96081e7dda01f2a5fa33
Bill Tomczak <bill@ostraining.com>
2016-01-27 22:31:44 -0800

    Initial stubs for quiz content

--- src/admin/models/fields/questions.php
+++ src/admin/models/fields/questions.php
@@ -12,15 +12,21 @@ class OscampusFormFieldQuestions extends JFormField
 {
     protected function getInput()
     {
-        $value = $this->value;
-        if ($value && is_string($value)) {
-            $value = json_decode($value, true);
+        echo '<h4>Please don\'t mind the mess - this is a work in progress</h4>';
+
+        $html = array('<div>');
+        foreach ($this->value as $qkey => $question) {
+            $attribs = array(
+                'id'       => $this->id . '_' . $qkey,
+                'name'     => $this->name . '[' . $qkey . ']',
+                'value'    => $question['text'],
+                'disabled' => 'disabled',
+                'size'     => 75
+            );
+            $html[]  = '<div class="clr"></div>';
+            $html[]  = '<input ' . OscampusUtilitiesArray::toString($attribs) . '/>';
         }
 
-        echo '<pre>';
-        print_r($value);
-        echo '</pre>';
-
-        return '';
+        return join('', $html);
     }
 }
