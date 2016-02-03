c87a0f35b6851946c1cc750c9ef5d92ba3579586
Bill Tomczak <bill@ostraining.com>
2016-02-03 13:44:37 -0800

    question/answer deletion Issue #72

--- src/media/js/admin/quiz.js
+++ src/media/js/admin/quiz.js
@@ -6,11 +6,12 @@
     $.Oscampus.admin.quiz = {};
 
     $.Oscampus.admin.quiz.init = function() {
-        $('[class*=osc-add-],[class*=osc-delete-]')
+        $('[class*=osc-delete-]')
             .css('cursor', 'pointer')
             .on('click', function(evt) {
                 evt.preventDefault();
-                alert('Under Construction');
+
+                $(this).parent('li').remove();
             });
     };
 })(jQuery);
