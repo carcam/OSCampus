890fe09932ac8733fdb7a5f6195bca96ba912744
Valentín <htmgarcia@gmail.com>
2016-02-03 15:13:01 -0600

    Signup overlay
    Close #64

--- src/site/views/lesson/tmpl/default_noauth.php
+++ src/site/views/lesson/tmpl/default_noauth.php
@@ -19,13 +19,44 @@ if ($itemid = (int)$this->getParams()->get($signupType)) {
 }
 
 ?>
-<div id="signup_overlay">
-    <div class="wrapper">
-        <h1>Become a member to view this session</h1>
+<style>
+    #oscampus .oscampus-lesson-content {
+        background: #000;
+        position: relative;
+    }
+    #oscampus .oscampus-lesson-content img {
+        opacity: 0.5;
+        width: 100%;
+        height: auto;
+    }
+    #oscampus .oscampus-lesson-content h3 {
+        margin-top: 0;
+        margin-bottom: 10px;
+    }
+    #oscampus #signup-overlay {
+        position: absolute;
+        top: 30px;
+        left: 30px;
+        width: 90%;
+    }
+    #oscampus .osc-overlay-inner {
+        background: #fff;
+        padding: 30px;
+        width: 60%;
+        border-radius: 4px;
+        -moz-border-radius: 4px;
+        -webkit-border-radius: 4px;
+        -o-border-radius: 4px;
+    }
+</style>
+
+<div id="signup-overlay">
+    <div class="osc-overlay-inner">
+        <h3>Become a member to view this session</h3>
         <?php
         if (!empty($signupPage)) :
             ?>
-            <p><?php echo JHtml::_('link', $signupPage, 'Signup here!'); ?></p>
+            <div><?php echo JHtml::_('link', $signupPage, 'Signup here!', 'class="osc-btn osc-btn-main"'); ?></div>
             <?php
         endif;
         ?>
@@ -34,14 +65,6 @@ if ($itemid = (int)$this->getParams()->get($signupType)) {
 
 <script>
     (function($) {
-        var parent = $('#oscampus');
-
-        var overlay = $('#signup_overlay')
-            .css({
-                width : parent.width(),
-                height: parent.height()
-            });
-
-        parent.prepend(overlay);
+        $('#signup-overlay').appendTo('.oscampus-lesson-content');
     })(jQuery);
 </script>
