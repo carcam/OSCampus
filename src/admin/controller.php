8f07be9a3ce430cb23695167603011187ffa3082
Bill Tomczak <bill@ostraining.com>
2016-02-05 11:42:50 -0800

    Temporary task to rewrite all URLs in DB from guru to ours Issue #92

--- src/admin/controller.php
+++ src/admin/controller.php
@@ -11,4 +11,63 @@ defined('_JEXEC') or die();
 class OscampusController extends OscampusControllerBase
 {
     protected $default_view = 'courses';
+
+    /**
+     * Temporary task to update old guru URLs to new OSCampus URLs
+     */
+    public function dbfix()
+    {
+        $db = OscampusFactory::getDbo();
+
+        $start = microtime(true);
+
+        $tables = $db->setQuery('show tables')->loadColumn();
+        foreach ($tables as $table) {
+            $idField = $db->setQuery("show columns from {$table} where `Key` = 'PRI' AND `Extra` = 'auto_increment'")
+                ->loadColumn();
+            if ($error = $db->getErrorMsg()) {
+                echo 'ERROR: ' . $error;
+                return;
+            }
+
+            if (count($idField) == 1) {
+                $fields = $db->setQuery("show columns from {$table} where type = 'text'")->loadColumn();
+
+                $idField = $idField[0];
+                array_unshift($fields, $idField);
+
+
+                $where = array();
+                foreach ($fields as $field) {
+                    $where[] = $db->quoteName($field) . ' RLIKE ' . $db->quote('/?courses/(categories|class|session)/');
+                }
+                $query = $db->getQuery(true)
+                    ->select($fields)
+                    ->from($table)
+                    ->where($where, 'OR');
+
+                if ($rows = $db->setQuery($query)->loadAssocList()) {
+                    echo '<br/>FIXING: ' . $table . ' - ' . count($rows) . ' Rows';
+                    foreach ($rows as $row) {
+                        foreach ($row as $fieldName => $value) {
+                            if ($fieldName != $idField && $value) {
+                                $row[$fieldName] = preg_replace('#/?(courses/)(categories|class|session)/#ms', '/classes/',
+                                    $value);
+                            }
+                            $update = (object)$row;
+
+                            $db->updateObject($table, $update, $idField);
+                            if ($error = $db->getErrorMsg()) {
+                                echo 'ERROR: ' . $error;
+                                return;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        echo '<p>Total Runtime: ' . number_format((microtime(true) - $start), 1) . '</p>';
+    }
 }
