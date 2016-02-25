8ee3c6438b5ef1bc053bff49f095c236197fc4a6
Bill Tomczak <bill@ostraining.com>
2016-02-24 17:47:01 -0800

    Initial setup for files admin

---
+++ src/admin/models/fields/files.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package    OSCampus
+ * @contact    www.ostraining.com, support@ostraining.com
+ * @copyright  2016 Open Source Training, LLC. All rights reserved
+ * @license
+ */
+
+defined('_JEXEC') or die();
+
+class OscampusFormFieldFiles extends JFormFieldText
+{
+}
