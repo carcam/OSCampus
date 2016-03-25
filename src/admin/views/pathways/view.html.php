741dd4896dd763b7d86451cdcc0f93346cb6d87b
Bill Tomczak <bill@ostraining.com>
2016-03-25 14:45:28 -0700

    Remove obsolete J2 code

--- src/admin/views/pathways/view.html.php
+++ src/admin/views/pathways/view.html.php
@@ -18,40 +18,5 @@ class OscampusViewPathways extends OscampusViewList
 
         $enableOrdering = $state->get('list.ordering') == 'pathway.ordering';
         $this->setOrdering('pathway.ordering', 'pathways', $enableOrdering);
-
-        $published = JHtml::_(
-            'osc.select.published',
-            'filter_published',
-            $state->get('filter.published'),
-            'COM_OSCAMPUS_OPTION_SELECT_PUBLISHED'
-        );
-
-        $owners = JHtml::_(
-            'osc.select.pathwayowner',
-            'filter_owner',
-            $state->get('filter.owner'),
-            array(
-                ''  => 'COM_OSCAMPUS_OPTION_SELECT_PATHWAY_OWNER',
-                '0' => 'COM_OSCAMPUS_OPTION_CORE_PATHWAY'
-            )
-        );
-
-        $access = JHtml::_(
-            'osc.select.access',
-            'filter_access',
-            $state->get('filter.access'),
-            'COM_OSCAMPUS_OPTION_SELECT_ACCESS'
-        );
-
-        $filters = array(
-            'text'  => array(
-                'value' => $this->getState()->get('filter.search')
-            ),
-            'items' => array(
-                array($published, $owners, $access)
-            )
-        );
-
-        $this->setVariable('filters', $filters);
     }
 }
