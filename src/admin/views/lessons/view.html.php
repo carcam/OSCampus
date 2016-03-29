9696b1ae9eb7e255e188d4362cc38b32474d303a
Bill Tomczak <bill@ostraining.com>
2016-03-29 12:54:30 -0700

    J3 makes grouped sorting much simpler Closes #228

--- src/admin/views/lessons/view.html.php
+++ src/admin/views/lessons/view.html.php
@@ -14,30 +14,9 @@ class OscampusViewLessons extends OscampusViewList
     {
         parent::setup();
 
-        $this->setOrdering('lesson.ordering', 'lessons', $this->enableOrdering());
-    }
+        $ordering = $this->getState()->get('list.ordering');
 
-    /**
-     * Determine whether it's okay to enable ordering
-     *
-     * @return bool
-     */
-    protected function enableOrdering()
-    {
-        $state = $this->getState();
-
-        $enabled = false;
-        if ($state->get('list.ordering') == 'lesson.ordering') {
-            $enabled = true;
-            $stateVars = $state->getProperties();
-            foreach ($stateVars as $name => $value) {
-                if ($value && strpos($name, 'filter.') === 0 && $name != 'filter.course') {
-                    return false;
-                }
-            }
-        }
-
-        return $enabled;
+        $this->setOrdering('lesson.ordering', 'lessons', $ordering == 'lesson.ordering');
     }
 
     public function getSortGroupId($item)
