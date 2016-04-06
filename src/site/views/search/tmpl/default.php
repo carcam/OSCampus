f8e2ab3d7ab9b8a95cc0b9bd5c83774c46624357
Bill Tomczak <bill@ostraining.com>
2016-04-06 13:16:24 -0700

    Formatting fixes

--- src/site/views/search/tmpl/default.php
+++ src/site/views/search/tmpl/default.php
@@ -11,20 +11,24 @@ defined('_JEXEC') or die();
 /** @var OscampusViewNewcourses $this */
 ?>
 <div class="<?php echo $this->getPageClass('osc-container oscampus-search'); ?>" id="oscampus">
-    <?php if ($heading = $this->getHeading('COM_OSCAMPUS_HEADING_SEARCH')): ?>
+    <?php
+    if ($heading = $this->getHeading('COM_OSCAMPUS_HEADING_SEARCH')) :
+        ?>
         <div class="page-header">
             <h1><?php echo $heading; ?></h1>
         </div>
-    <?php endif; ?>
+        <?php
+    endif;
+    ?>
 
     <?php
     if ($this->items) :
-        foreach ($this->items as $this->item) {
+        foreach ($this->items as $this->item) :
             // By default uses sub-template from pathway view
             echo $this->loadTemplate('course');
-        }
+        endforeach;
 
-    else:
+    else :
         ?>
         <div class="osc-alert-notify"><i class="fa fa-info-circle"></i>
             <?php echo JText::_('COM_OSCAMPUS_PATHWAY_NO_COURSES'); ?>
