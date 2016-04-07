14533a737ab736c950bd81957210436b6f245ad5
Bill Tomczak <bill@ostraining.com>
2016-04-07 10:15:31 -0700

    Fill in the lesson results display Issue #246

--- src/site/views/search/tmpl/default_lessons.php
+++ src/site/views/search/tmpl/default_lessons.php
@@ -14,8 +14,23 @@ if ($this->lessons) :
         <?php echo JText::plural('COM_OSCAMPUS_SEARCH_RESULTS_LESSONS', count($this->lessons)); ?>
     </div>
     <?php
-    foreach ($this->lessons as $this->item) :
-        echo '<br/>' . $this->item->title;
+    foreach ($this->lessons as $item) :
+        ?>
+        <div class="osc-section osc-course-list">
+            <div class="block4 osc-course-image">
+                <?php
+                $link  = JHtml::_('osc.link.lessonid', $item->courses_id, $item->id, null, null, true);
+                $image = JHtml::_('image', \Oscampus\Course::DEFAULT_IMAGE, $item->title);
+                echo JHtml::_('link', $link, $image);
+                ?>
+            </div>
+            <div class="block8 osc-course-description">
+                <h2><?php echo JHtml::_('link', $link, $item->title); ?></h2>
+            </div>
+        </div>
+        <!-- .osc-section -->
+
+        <?php
     endforeach;
 
 else :
