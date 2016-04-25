6f5a6ae96aef8f6e7f826a6bf86326bcb8c0764a
Bill Tomczak <bill@ostraining.com>
2016-04-25 10:44:38 -0700

    Establish options for the course layout Closes #255

--- src/site/layouts/course.php
+++ src/site/layouts/course.php
@@ -15,26 +15,31 @@ defined('_JEXEC') or die();
  * @var string      $path
  */
 
-$item  = $displayData;
-$link  = JRoute::_(JHtml::_('osc.course.link', $item, null, null, true));
-$image = JHtml::_('image', $item->image, $item->title);
+$item    = $displayData;
+$link    = JRoute::_(JHtml::_('osc.course.link', $item, null, null, true));
+$image   = JHtml::_('image', $item->image, $item->title);
+$options = $this->getOptions();
 
+if ($options->get('image', true)) :
+    ?>
+    <div class="osc-section osc-course-item">
+        <div class="block4 osc-course-image">
+            <?php echo JHtml::_('link', $link, $image); ?>
+        </div>
+        <div class="block8 osc-course-description">
+            <h2><?php echo JHtml::_('link', $link, $item->title); ?></h2>
+            <?php echo $item->introtext ?: $item->description; ?>
+        </div>
+    </div>
+    <?php
+endif;
 ?>
-<div class="osc-section osc-course-item">
-    <div class="block4 osc-course-image">
-        <?php echo JHtml::_('link', $link, $image); ?>
-    </div>
-    <div class="block8 osc-course-description">
-        <h2><?php echo JHtml::_('link', $link, $item->title); ?></h2>
-        <?php echo $item->introtext ?: $item->description; ?>
-    </div>
-</div>
 <!-- .osc-section -->
 
 <div class="osc-section osc-course-list">
     <div class="block9">
         <?php
-        if ($item->tags) :
+        if ($options->get('tags', true) && $item->tags) :
             ?>
             <span class="osc-label">
                 <i class="fa fa-tag"></i>
@@ -42,18 +47,42 @@ $image = JHtml::_('image', $item->image, $item->title);
             </span>
             <?php
         endif;
+
+        if ($options->get('lessonCount', true)) :
+            ?>
+            <span class="osc-label">
+                <i class="fa fa-list"></i>
+                <?php echo JText::plural('COM_OSCAMPUS_COURSE_LESSON_COUNT', $item->lesson_count); ?>
+            </span>
+            <?php
+        endif;
+
+        if ($options->get('length', true)) :
+            ?>
+            <span class="osc-label">
+                <i class="fa fa-clock-o"></i>
+                <?php echo JText::plural('COM_OSCAMPUS_COURSE_LENGTH_MINUTES', $item->length); ?>
+            </span>
+            <?php
+        endif;
+
+        if ($options->get('released', false)) :
+            ?>
+            <span class="osc-label">
+                <i class="fa fa-calendar"></i>
+                <?php echo date('F j, Y', strtotime($item->released)); ?>
+          </span>
+            <?php
+        endif;
+
+        if ($options->get('teacher', true)) :
+            ?>
+            <span class="osc-label">
+                <i class="fa fa-user"></i> <?php echo $item->teacher; ?>
+            </span>
+            <?php
+        endif;
         ?>
-        <span class="osc-label">
-            <i class="fa fa-list"></i>
-            <?php echo JText::plural('COM_OSCAMPUS_COURSE_LESSON_COUNT', $item->lesson_count); ?>
-        </span>
-        <span class="osc-label">
-            <i class="fa fa-clock-o"></i>
-            <?php echo JText::plural('COM_OSCAMPUS_COURSE_LENGTH_MINUTES', $item->length); ?>
-        </span>
-        <span class="osc-label">
-            <i class="fa fa-user"></i> <?php echo $item->teacher; ?>
-        </span>
     </div>
     <div class="block3 osc-course-start">
         <?php echo JHtml::_('osc.course.startbutton', $item); ?>
