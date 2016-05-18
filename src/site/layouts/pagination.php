170d790129b6475b3afe70819d2734229c294f81
Valentín <htmgarcia@gmail.com>
2016-05-18 16:37:11 -0500

    Core pagination classes
    Issue #269

--- src/site/layouts/pagination.php
+++ src/site/layouts/pagination.php
@@ -26,20 +26,20 @@ if (!empty($pages)) {
     $displayLink = function (JPaginationObject $page, $title = null) {
         $text = $title ?: $page->text;
         if ($page->active) {
-            return '<li class="uk-active"><span>' . $text . '</span></li>';
+            return '<li class="active"><span>' . $text . '</span></li>';
         }
 
         return '<li>' . JHtml::_('link', $page->link, $text) . '</li>';
     };
     ?>
     <div class="pagination">
-        <ul class="uk-pagination">
+        <ul class="pagination-list">
             <?php
             if ($pages['start']['active']) {
-                echo $displayLink($pages['start']['data'], '<i class="uk-icon-angle-double-left"></i>');
+                echo $displayLink($pages['start']['data'], '<i class="icon-first"></i>');
             }
             if ($pages['previous']['active']) {
-                echo $displayLink($pages['previous']['data'], '<i class="uk-icon-angle-left"></i>');
+                echo $displayLink($pages['previous']['data'], '<i class="icon-previous"></i>');
             }
 
             foreach ($pages['pages'] as $page) {
@@ -47,10 +47,10 @@ if (!empty($pages)) {
             }
 
             if ($pages['next']['active']) {
-                echo $displayLink($pages['next']['data'], '<i class="uk-icon-angle-right"></i>');
+                echo $displayLink($pages['next']['data'], '<i class="icon-next"></i>');
             }
             if ($pages['end']['active']) {
-                echo $displayLink($pages['end']['data'], '<i class="uk-icon-angle-double-right"></i>');
+                echo $displayLink($pages['end']['data'], '<i class="icon-last"></i>');
             }
             ?>
         </ul>
